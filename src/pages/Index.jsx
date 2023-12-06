80c66b8a650ab8f36903164b981bd11637b69af2
tomkjose <tomjosekunnumpurath@gmail.com>
2023-12-06 17:26:31 +0530

    latest commit

--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -2,7 +2,6 @@ import Home from "./Home";
 import Fof from "./Fof";
 import Signin from "./Signin";
 import Signup from "./Signup";
-import Favourite from "./Favourite";
+import Favourite from "./favourite";
 import Post from "./Post";
 export { Home, Signin, Signup, Post, Favourite, Fof };
-//Favourite
