4645e7d6758e95e13362c81a294d55c658b46b0c
tomkjose <tomjosekunnumpurath@gmail.com>
2024-01-05 12:13:54 +0530

    Change API

--- src/utils/constants/index.jsx
+++ src/utils/constants/index.jsx
@@ -1,5 +1,4 @@
-const API_KEY = process.env.REACT_APP_API_KEY;
 const FIREBASE_KEY = process.env.REACT_APP_FIREBASE_KEY;
-export const API_URL = `https://newsapi.org/v2/everything?q=apple&from=2023-12-04&to=2023-12-04&sortBy=popularity&apiKey=${API_KEY}`;
+export const API_URL = `https://new-backend-hbz4.onrender.com/news`;
 export const SIGN_IN_API = `https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${FIREBASE_KEY}`;
 export const SIGN_UP_API = `https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${FIREBASE_KEY}`;
